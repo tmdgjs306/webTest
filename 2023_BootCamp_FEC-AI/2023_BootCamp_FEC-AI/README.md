389d6ee84a4d1619391cd8902d96e7a72877cae9
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2023-08-22 19:21:13 +0900

    test1

---
+++ 2023_BootCamp_FEC-AI/2023_BootCamp_FEC-AI/README.md
@@ -0,0 +1,23 @@
+# 2023_BootCamp_FEC
+ 스마트 팜 제어 웹페이지 제작 프로젝트   
+- 구현기능
+   - 로그인 세션
+   - LED 제어
+   - 센서 측정 값 모니터링 기능  
+
+위 프로젝트는 2023 SW전문인재양성사업 기업주도 프로젝트의 일환으로 개발된 프로젝트입니다. 
+
+# Tech Stack 
+ - Back_End 
+   - Spring Boot 3.0
+   - MySql
+    
+ - Front_End
+   - HTML 5 
+   - CSS 3
+   - Thymeleaf
+ 
+ - Device 
+   - Arduino ESP32 (ET Board)
+   - C++
+    
---
+++ 2023_BootCamp_FEC-AI/2023_BootCamp_FEC-AI/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from roboflow import Roboflow
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+CORS(app)
+
+rf = Roboflow(api_key="oxklL1EcEBgoWd2Au4Zv")
+project = rf.workspace("fec").project("fec")
+model = project.version(1).model
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' in request.files:
+        image = request.files['image']
+        if image.filename == '':
+            return jsonify({"error": "No selected file"})
+
+        filename = secure_filename(image.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image.save(filepath)
+
+        prediction_result = model.predict(filepath, confidence=50, overlap=50).json()
+        filtered_predictions = [item for item in prediction_result['predictions']]
+        if filtered_predictions:
+            predicted_class = filtered_predictions[0]['class']
+            return jsonify({"class": predicted_class})
+        else:
+            return jsonify({"class": "Unknown"})  # No prediction above the threshold
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
